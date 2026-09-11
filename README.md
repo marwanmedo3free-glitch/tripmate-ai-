c569902eb43e9309eb4dda17ad552397bea405f5
marwan ahmed <marwanmedo3free@gmail.com>
2026-09-11 12:07:24 +0300

    done

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # tripmate-ai-
 
-
-postgresql://authenticator:npg_D08NtdywIYaO@ep-royal-paper-aevozpf7-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
+# res = search_flights("plan a 6 days egypt trip from DAC to CAI in December 2024 for 2 adults and 1 child, including flights, hotels, and sightseeing activities. Provide a detailed itinerary with estimated costs and travel tips.")
+# print(res)
 
-res = search_flights("plan a 6 days egypt trip from DAC to CAI in December 2024 for 2 adults and 1 child, including flights, hotels, and sightseeing activities. Provide a detailed itinerary with estimated costs and travel tips.")
-print(res)
+user_input=input("Enter your travel query: ")
+res= run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+print(res["answer"])
